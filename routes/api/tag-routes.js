3a3e99d42f51b19c8d283ffed016c7f7e03df8f1
Jeffreydne <Jeffreydne@gmail.com>
2024-02-03 10:00:56 -0800

    add functionality to tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,13 +3,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags, including its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: {model: Product, through: ProductTag, as: 'product_to_be_tagged' },
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`, including its associated Product data
    // XXXX ALSO  // ADD if statement see activity 28 travelerRoutes.js line 52 XXXXXXXXXXXXXXX
+   try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: {model: Product, through: ProductTag, as: 'product_to_be_tagged' },
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
@@ -21,9 +37,19 @@ router.put('/:id', (req, res) => {
    // XXXX ALSO  // ADD if statement see activity 28 travelerRoutes.js line 52 XXXXXXXXXXXXXXX
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
    // XXXX ALSO  // ADD if statement see activity 28 travelerRoutes.js line 52 XXXXXXXXXXXXXXX
+   try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+       },
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
