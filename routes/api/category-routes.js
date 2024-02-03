c5b1912c8294b7c22591e620178a6d96f6d6921d
Jeffreydne <Jeffreydne@gmail.com>
2024-02-02 18:53:13 -0800

    complete category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,66 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // find all categories, include associated products for each category
+  try {
+    const catData = await Category.findAll({
+      include: { model: Product },
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value & include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: { model: Product },
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+   try {
+    const catData = await Category.create(req.body);
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  //XXXXXXX DOES THIS PUT WORK????? XXXXXXXXX
+   try {
+    const catData = await Category.update(
+      { category_name: req.params.id}, 
+      { where: req.params.id },
+    );
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+   try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id
+       },
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
